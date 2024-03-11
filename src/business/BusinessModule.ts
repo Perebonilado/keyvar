161a1d4eb808988d7d7b775d2a2b2fb33360ed95
perebonilado <reradiri@eminenttechnology.com>
2024-03-11 14:53:08 +0100

    handler registration

--- src/business/BusinessModule.ts
+++ src/business/BusinessModule.ts
@@ -4,6 +4,7 @@ import { QueryModule } from 'src/query/QueryModule';
 import CreateNewsInsightSubscriberHandler from './handlers/NewsInsight/CreateNewsInsightSubscriberHandler';
 import KeyvarEventEmitter from './events/KeyvarEventEmitter';
 import { CreateBusinessLeadHandler } from './handlers/BusinessLead/CreateBusinessLeadHandler';
+import { CreateBusinessEnquiryHandler } from './handlers/BusinessEnquiry/CreateBusinessEnquiryHandler';
 
 @Module({
   imports: [QueryModule, InfraRepositoryModule],
@@ -11,11 +12,13 @@ import { CreateBusinessLeadHandler } from './handlers/BusinessLead/CreateBusines
     KeyvarEventEmitter,
     CreateNewsInsightSubscriberHandler,
     CreateBusinessLeadHandler,
+    CreateBusinessEnquiryHandler,
   ],
   exports: [
     KeyvarEventEmitter,
     CreateNewsInsightSubscriberHandler,
     CreateBusinessLeadHandler,
+    CreateBusinessEnquiryHandler,
   ],
 })
 export class BusinessModule {}
