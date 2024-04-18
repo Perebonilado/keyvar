426d84b9b4ac33dae48c20634326b6a8a9fc4a5b
perebonilado <reradiri@eminenttechnology.com>
2024-04-19 00:18:29 +0100

    app fix

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import { EnvironmentVariables } from './EnvironmentVariables';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(EnvironmentVariables.config.port);
-  console.log(`Server running on port: ${EnvironmentVariables.config.port}`);
+  await app.listen(EnvironmentVariables.config.port, '0.0.0.0', () => {
+    console.log(`Server running on port: ${EnvironmentVariables.config.port}`);
+  });
 }
 bootstrap();
