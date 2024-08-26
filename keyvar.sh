fb961b1a07a2f65ee4627abc9147373402b7391e
perebonilado <perebonilado@gmail.com>
2024-08-26 23:27:39 +0100

    script updt 2

--- keyvar.sh
+++ keyvar.sh
@@ -3,7 +3,7 @@
 FILE_PATH=".env"
 
 NEW_EMAIL_USER="Email_user = support@keyvarsolutions.com"
-NEW_EMAIL_PASS="Email_pass = uwrs nbvj bslp ugnyz"
+NEW_EMAIL_PASS="Email_pass = uwrs nbvj bslp ugny"
 
 sed -i "s/^Email_user = .*/$NEW_EMAIL_USER/" $FILE_PATH
 sed -i "s/^Email_pass = .*/$NEW_EMAIL_PASS/" $FILE_PATH
